9d6f2640e758c0c18dc182c917272bb7e65abbcb
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-05-05 19:29:42 -0500

    update

---
+++ src/org/launchcode/studio7/OpticalDisc.java
@@ -0,0 +1,9 @@
+package org.launchcode.studio7;
+
+public interface OpticalDisc {
+    String spinDisc();
+    String storeData();
+    String dataWrittenByLaser();
+    String dataReadByLaser();
+    String reportData();
+}
